baa2eac297bec62efb5ee905199a32c3e74ab92a
halaszruben <ruben_halasz12@yahoo.com>
2022-12-10 17:50:10 +0200

    "Initial Commit"

---
+++ src/test/java/inf/unideb/hu/WebProject/WebProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package inf.unideb.hu.WebProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
