755994593c4310e5026230f4c7e3cde3b2f29b8c
halaszruben <ruben_halasz12@yahoo.com>
2022-12-14 18:26:26 +0200

    Little change to UI

--- src/main/webapp/frontend/src/components/HeaderComponent.js
+++ src/main/webapp/frontend/src/components/HeaderComponent.js
@@ -18,6 +18,8 @@ class HeaderComponent extends Component {
                     <nav className="navbar navbar-expand-md navbar-dark bg-dark">
                         <div>
                             <a href="https://github.com/halaszruben/WebDevelopment-Project" className="navbar-brand">Github Repo</a>
+                            <a href='http://localhost:3000/' style={{ marginLeft: "10px", marginRight: "10px" }}>Home</a>
+                            <a href='http://localhost:3000/add-book' style={{ marginLeft: "10px", marginRight: "10px" }}>Add Books</a>
                         </div>
                     </nav>
                 </header>
